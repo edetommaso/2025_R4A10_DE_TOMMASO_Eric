17db666d623107fc308b32b76713ed7ee00135d3
Eric <eric.detommaso@gmail.com>
2025-02-12 20:40:52 +0100

    debuggage

--- tp2/src/app/header/header.component.ts
+++ tp2/src/app/header/header.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 
 export class HeaderComponent {
   title = "Trafalgar";
-
+  
   menus = [
     { label: "Home", href: "#" },
     { label: "Find a doctor", href: "#" },
